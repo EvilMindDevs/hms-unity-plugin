bc2a8212858239237f4b1cf79201134d2ff7513c
mmontesinos81 <miguel.montesinos@evil-mind.com>
2020-04-23 19:44:58 +0200

    IAP:
    get productlists and info.
    BuyPurchase and consume

---
+++ Assets/Huawei/Scripts/HuaweiConstants.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HuaweiConstants
+{
+    public static class IAP
+    {
+        public enum IapType :int
+        {
+            CONSUMABLE = 0,
+            NON_CONSUMABLE = 1,
+            SUBSCRIPTION = 2
+        }
+        
+    }
+    public static class HMSResponses
+    {
+
+        public enum GameStatusCodes
+        {
+            GAME_STATE_SUCCESS = 0,
+            GAME_STATE_FAILED = -1,
+            GAME_STATE_ERROR = 7001,
+            GAME_STATE_NETWORK_ERROR = 7002,
+            GAME_STATE_USER_CANCEL = 7003,
+            GAME_STATE_USER_CANCEL_LOGIN = 7004,
+            GAME_STATE_PARAM_ERROR = 7005,
+            GAME_STATE_NO_SUPPORT = 7006,
+            GAME_STATE_PARAM_NULL = 7011,
+            GAME_STATE_CALL_REPEAT = 7012,
+            GAME_STATE_NOT_LOGIN = 7013,
+            GAME_STATE_DISAGREE_PROTOCOL = 7014
+
+        }
+
+        public enum PayStatusCodes
+        {
+            PAY_STATE_SUCCESS = 0,
+            PAY_STATE_FAILED = -1,
+            PAY_STATE_CANCEL = 30000,
+            PAY_STATE_PARAM_ERROR = 30001,
+            PAY_STATE_TIME_OUT = 30002,
+            PAY_STATE_NET_ERROR = 30005,
+            PAY_STATE_ERROR = 30099,
+            PAY_OTHER_ERROR = 30006,
+            HWPAY_COUNTRY_CODE_ERROR = 30011,
+            ORDER_STATUS_HANDLING = 30012,
+            ORDER_STATUS_UNTREATED = 30013,
+            PAY_GAME_ACCOUNT_ERROR = 30101,
+            PRODUCT_NOT_EXIST = 40001,
+            PRODUCT_AUTHENTICATION_FAILED = 40002,
+            PRODUCT_SERVER_INTERNAL_EXCEPTION = 40003,
+            PRODUCT_SOME_NOT_EXIST = 40004
+
+        }
+    }
+
+}
---
+++ Assets/Huawei/Scripts/IAP/IapManager.cs
@@ -0,0 +1,315 @@
+﻿# define DEBUG
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HuaweiConstants;
+using HuaweiMobileServices.Base;
+
+using HuaweiMobileServices.IAP;
+using System;
+using UnityEngine.Events;
+
+public class IapManager : MonoBehaviour
+{
+
+    public string key;
+
+
+    public Product[] products;
+
+    [HideInInspector]
+    public int numberOfProductsRetrieved;
+
+    List<string> productIdConsumablesList = new List<string>();
+    List<string> productIdNonConsumablesList = new List<string>();
+    List<ProductInfo> productInfoList = new List<ProductInfo>();
+
+    IIapClient iapClient;
+
+    UnityEvent loadedEvent;
+
+
+    void Awake()
+    {
+#if DEBUG
+        Debug.Log("[HMSPlugin]: IAPP manager Init");
+#endif
+        Debug.Log("[HMSPlugin]: IAP manager Init");
+        loadedEvent = new UnityEvent();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitIAP();
+        // RestorePurchases();
+        recoverPurchases();
+
+        
+
+
+    }
+
+    
+
+
+
+    private void InitIAP()
+    {
+        foreach (Product product in products)
+        {
+            if (product.isConsumable)
+            {
+                productIdConsumablesList.Add(product.productID);
+            }
+
+        }
+
+        ProductInfoReq productInfoReq = new ProductInfoReq();
+       
+        iapClient = Iap.GetIapClient();
+     
+
+        // CONSUMABLE REQUEST
+        productInfoReq.PriceType = 0;
+    
+        productInfoReq.ProductIds = productIdConsumablesList;
+        
+        ITask<ProductInfoResult> task = iapClient.ObtainProductInfo(productInfoReq);
+        
+        Debug.Log(task.Result);
+
+        task.AddOnSuccessListener((result) =>
+        {
+
+            if (result != null)
+            {
+                
+                if (result.ProductInfoList.Count != 0)
+                {
+                    
+                    Debug.Log("[HMSPlugin]:" + result.ErrMsg + result.ReturnCode.ToString());
+                    Debug.Log("[HMSPlugin]: Found " + result.ProductInfoList.Count + "consumable products");
+                    foreach (ProductInfo productInfo in result.ProductInfoList)
+                    {
+                        Debug.Log("[HMSPlugin]: " + productInfo.ProductName + "," + productInfo.Price);
+                        productInfoList.Add(productInfo);
+                    }
+                }
+
+
+            }
+            loadedEvent.Invoke();
+
+        }).AddOnFailureListener((exception) =>
+        {
+            Debug.Log("[HMSPlugin]: ERROR Houston!!" + exception.Message);
+        });
+
+        
+
+        RestorePurchases();
+    }
+
+
+    private void RestorePurchases()
+    {
+        foreach (Product product in products)
+        {
+            if (!product.isConsumable)
+            {
+                productIdConsumablesList.Add(product.productID);
+            }
+
+        }
+
+        ProductInfoReq productInfoReq = new ProductInfoReq();
+        
+
+        // NON CONSUMABLE REQUEST
+        productInfoReq.PriceType = 1;
+        productInfoReq.ProductIds = productIdConsumablesList;
+
+        ITask<ProductInfoResult> task = iapClient.ObtainProductInfo(productInfoReq);
+
+        task.AddOnSuccessListener((result) =>
+        {
+            Debug.Log("HMSPlugin: NON consumables result success");
+            if (result != null && result.ProductInfoList.Count != 0)
+            {
+                Debug.Log("[HMSPlugin]:" + result.ErrMsg + result.ReturnCode.ToString());
+                Debug.Log("[HMSPlugin]: Found " + result.ProductInfoList.Count + "non consumable products");
+                foreach (ProductInfo productInfo in result.ProductInfoList)
+                {
+                    Debug.Log("[HMSPlugin]: " + productInfo.ProductName + "," + productInfo.Price);
+                    productInfoList.Add(productInfo);
+                }
+
+            }
+
+        }).AddOnFailureListener((exception) =>
+        {
+            Debug.Log("[HMSPlugin]: ERROR Houston!!" + exception.Message);
+        });
+    }
+
+    public ProductInfo GetProductInfo(string productID)
+    {
+        return productInfoList.Find( productInfo => productInfo.ProductId == productID);
+    }
+
+    private void recoverPurchases()
+    {
+        OwnedPurchasesReq ownedPurchasesReq = new OwnedPurchasesReq();
+
+        ITask<OwnedPurchasesResult> task = iapClient.ObtainOwnedPurchases(ownedPurchasesReq);
+
+        task.AddOnSuccessListener((result) =>
+        {
+            Debug.Log("HMSP: recoverPurchases");
+            foreach(string inAppPurchaseData in result.InAppPurchaseDataList) {
+
+                InAppPurchaseData inAppPurchaseDataBean = new InAppPurchaseData(inAppPurchaseData);
+                string purchaseToken = inAppPurchaseDataBean.PurchaseToken;
+
+                ConsumePurchase(purchaseToken);
+
+                Debug.Log("HMSP: recoverPurchases result> " + result.ReturnCode);
+            }
+            
+            
+        }).AddOnFailureListener((exception) =>
+        {
+            Debug.Log("HMSP: Error on recoverPurchases"+ exception.StackTrace);
+        });
+    }
+
+
+    private void ConsumePurchase(string token) {
+
+        ConsumeOwnedPurchaseReq consumeOwnedPurchaseReq = new ConsumeOwnedPurchaseReq();
+        consumeOwnedPurchaseReq.PurchaseToken = token;
+
+        ITask<ConsumeOwnedPurchaseResult> task = iapClient.ConsumeOwnedPurchase(consumeOwnedPurchaseReq);
+
+        task.AddOnSuccessListener((result) =>
+        {
+            Debug.Log("HMSP: consumePurchase");
+
+        }).AddOnFailureListener((exception) =>
+        {
+            Debug.Log("HMSP: Error on consumePurchase");
+        });
+    }
+
+    public void BuyProduct(string productID)
+    {
+
+        PurchaseIntentReq purchaseIntentReq = new PurchaseIntentReq();
+        purchaseIntentReq.PriceType = 0;
+        purchaseIntentReq.ProductId = productID;
+        // ToDo : developer payload???
+        purchaseIntentReq.DeveloperPayload = "test";
+
+        ITask<PurchaseIntentResult> task = iapClient.CreatePurchaseIntent(purchaseIntentReq);
+
+        task.AddOnSuccessListener((result) =>
+        {
+
+            if (result != null)
+            {
+                Debug.Log("[HMSPlugin]:" + result.ErrMsg + result.ReturnCode.ToString());
+                Debug.Log("[HMSPlugin]: Bought " + productID);
+                Status status = result.Status;
+                status.StartResolutionForResult((androidIntent) =>
+                {
+                    PurchaseResultInfo purchaseResultInfo = iapClient.ParsePurchaseResultInfoFromIntent(androidIntent);
+
+                    Debug.Log("HMSPluginResult: " + purchaseResultInfo.ReturnCode);
+
+                    Debug.Log("HMSPluginPurchaseData: " + purchaseResultInfo.InAppPurchaseData);
+
+
+                    
+                   
+
+                    switch (purchaseResultInfo.ReturnCode)
+                    {
+                        case OrderStatusCode.ORDER_STATE_CANCEL:
+                            // User cancel payment.
+                            break;
+                        case OrderStatusCode.ORDER_STATE_FAILED:
+
+                        case OrderStatusCode.ORDER_PRODUCT_OWNED:
+                            // to check if there exists undelivered products.
+                            break;
+                        case OrderStatusCode.ORDER_STATE_SUCCESS:
+                            // pay success.
+                            String inAppPurchaseData = purchaseResultInfo.InAppPurchaseData;
+                            String inAppPurchaseDataSignature = purchaseResultInfo.InAppDataSignature;
+                            Debug.Log("HMS:" + inAppPurchaseData);
+                            Debug.Log("HMS:" + inAppPurchaseDataSignature);
+                            // use the public key of your app to verify the signature.
+                            // If ok, you can deliver your products.
+                            // If the user purchased a consumable product, call the consumeOwnedPurchase API to consume it after successfully delivering the product.
+                            break;
+                        default:
+                            break;
+                    }
+
+                    onPurchaseSuccess();
+                },(exception) =>
+                {
+                    onPurchaseError();
+                }
+                );
+
+
+            }
+
+        }).AddOnFailureListener((exception) =>
+        {
+            Debug.Log("[HMSPlugin]: ERROR BuyProduct!!" + exception.Message);
+        });
+
+
+
+    }
+
+    public void onPurchaseSuccess()
+    {
+        Debug.Log("[HMSPlugin]: PURCHASE SUCCESS");
+    }
+
+    public void onPurchaseError()
+    {
+        Debug.Log("[HMSPlugin]: PURCHASE ERROR");
+    }
+
+
+    public void addListener(UnityAction action){
+        if (loadedEvent!= null)
+        {
+            loadedEvent.AddListener(action);  
+        } 
+        
+    }
+
+
+
+
+}
+
+
+
+
+[System.Serializable]
+public class Product
+{
+    [SerializeField]
+    public string productID;
+    [SerializeField]
+    public bool isConsumable;
+}

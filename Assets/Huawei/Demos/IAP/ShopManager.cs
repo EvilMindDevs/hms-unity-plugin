f8862f54dadea82375832dae8930e3b1f4a6ff51
Yassine Diouri <ametric@gmail.com>
2020-05-04 13:16:59 +0200

    Comment out code that doesn't compile

--- Assets/Huawei/Demos/IAP/ShopManager.cs
+++ Assets/Huawei/Demos/IAP/ShopManager.cs
@@ -1,105 +1,107 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class ShopManager : MonoBehaviour
-{
-    private GameObject shopItemsGO;
+//public class ShopManager : MonoBehaviour
+//{
+//    private GameObject shopItemsGO;
 
-    private IapManager iapManager;
+//    private IapManager iapManager;
 
-    //UI
-    Text coinsText;
-    Text userText;
-    Button loginButton;
+//    //UI
+//    Text coinsText;
+//    Text userText;
+//    Button loginButton;
 
-    // Consumables
-    int coins;
+//    //// Consumables
+//    int coins;
 
-    //Non consumables
-    bool ads;
-    bool premium;
+//    //Non consumables
+//    bool ads;
+//    bool premium;
 
-    bool logged;
+//    bool logged;
 
-    string user;
+//    string user;
 
-    ShopItem[] shopItems;
-
-
-    void Start()
-    {
-        iapManager = GameObject.Find("IapManager").GetComponent<IapManager>();
-
-        if (iapManager != null) {
-            Debug.Log("[HMSPlugin]: adding listener ");
-            iapManager.addListener(activateProducts);
-        } else
-        {
-            Debug.Log("[HMSPlugin]: NO IAP MANAGER IN SHOP");
-        }
-
-        coinsText = GameObject.Find("CoinsText").GetComponent<Text>();
-        userText = GameObject.Find("UserText").GetComponent<Text>();
-        loginButton = GameObject.Find("LoginButton").GetComponent<Button>();
-        shopItemsGO = GameObject.Find("ShopItems");
+//    ShopItem[] shopItems;
 
 
-        if (!logged)
-        {
-            userText.text = "not logged";
-        }
+//    void Start()
+//    {
+//        iapManager = GameObject.Find("IapManager").GetComponent<IapManager>();
 
-        
+//        if (iapManager != null)
+//        {
+//            Debug.Log("[HMSPlugin]: adding listener ");
+//            iapManager.addListener(activateProducts);
+//        }
+//        else
+//        {
+//            Debug.Log("[HMSPlugin]: NO IAP MANAGER IN SHOP");
+//        }
 
-    }
+//        coinsText = GameObject.Find("CoinsText").GetComponent<Text>();
+//        userText = GameObject.Find("UserText").GetComponent<Text>();
+//        loginButton = GameObject.Find("LoginButton").GetComponent<Button>();
+//        shopItemsGO = GameObject.Find("ShopItems");
 
 
-    void activateProducts()
-    {
-        Debug.Log("[HMSPlugin]: Loading products ");
-        shopItems = shopItemsGO.GetComponentsInChildren<ShopItem>();
-        foreach (ShopItem item in shopItems)
-        {
-            item.Load();
-        }
-    }
-
-    public void addCoins(int amount)
-    {
-        coins += amount;
-        coinsText.text = coins.ToString();
-    }
-
-    //public void setNoAds(bool purchased)
-    //{
-    //    ads = purchased;
-
-    //    NonConsumableButton[] buttons = transform.GetComponentsInChildren<NonConsumableButton>();
-    //    foreach (NonConsumableButton button in buttons)
-    //    {
-    //        button.alreadyBought("no_ads");
-    //    }
-    //}
-
-    //public void setPremium(bool purchased)
-    //{
-    //    premium = purchased;
-
-    //    NonConsumableButton[] buttons = transform.GetComponentsInChildren<NonConsumableButton>();
-    //    foreach (NonConsumableButton button in buttons)
-    //    {
-    //        button.alreadyBought("premium");
-    //    }
-    //}
+//        if (!logged)
+//        {
+//            userText.text = "not logged";
+//        }
 
 
 
-    public void setUser(string name)
-    {
-        userText.text = name;
-        logged = true;
-        loginButton.interactable = false;
-    }
-}
+//    }
+
+
+//    void activateProducts()
+//    {
+//        Debug.Log("[HMSPlugin]: Loading products ");
+//        shopItems = shopItemsGO.GetComponentsInChildren<ShopItem>();
+//        foreach (ShopItem item in shopItems)
+//        {
+//            item.Load();
+//        }
+//    }
+
+//    public void addCoins(int amount)
+//    {
+//        coins += amount;
+//        coinsText.text = coins.ToString();
+//    }
+
+//    //public void setNoAds(bool purchased)
+//    //{
+//    //    ads = purchased;
+
+//    //    NonConsumableButton[] buttons = transform.GetComponentsInChildren<NonConsumableButton>();
+//    //    foreach (NonConsumableButton button in buttons)
+//    //    {
+//    //        button.alreadyBought("no_ads");
+//    //    }
+//    //}
+
+//    //public void setPremium(bool purchased)
+//    //{
+//    //    premium = purchased;
+
+//    //    NonConsumableButton[] buttons = transform.GetComponentsInChildren<NonConsumableButton>();
+//    //    foreach (NonConsumableButton button in buttons)
+//    //    {
+//    //        button.alreadyBought("premium");
+//    //    }
+//    //}
+
+
+
+//    public void setUser(string name)
+//    {
+//        userText.text = name;
+//        logged = true;
+//        loginButton.interactable = false;
+//    }
+//}

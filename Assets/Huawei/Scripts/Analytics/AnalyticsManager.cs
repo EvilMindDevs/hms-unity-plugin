d7c7b6ec1b2d84048d7e594c45cb329d305f886f
y00429754 <yusufaltungoldtr@hotmail.com>
2020-09-16 18:33:32 +0300

    Analytics kit added

---
+++ Assets/Huawei/Scripts/Analytics/AnalyticsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HuaweiMobileServices.Analystics;
+using HuaweiMobileServices.Utils;
+using System;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    public static AnalyticsManager GetInstance(string name = "AnalyticsManager") => GameObject.Find(name).GetComponent<AnalyticsManager>();
+
+    private HiAnalyticsInstance instance;
+    void InitilizeAnalyticsInstane()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+        HiAnalyticsTools.enableLog();
+        instance = HiAnalytics.getInstance(activity);
+        instance.setAnalyticsEnabled(true);
+
+    }
+    public void SendEventWithBundle(String eventID, String key, String value)
+    {
+        Bundle bundleUnity = new Bundle();
+        bundleUnity.putString(key, value);
+        instance.onEvent(eventID, bundleUnity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitilizeAnalyticsInstane();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

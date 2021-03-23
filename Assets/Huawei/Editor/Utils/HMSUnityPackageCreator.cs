3729114d165c9ce6da2a0e41baa3cbb5944be1b0
Samet Güzel <sametguzeldev@gmail.com>
2021-03-24 02:31:40 +0300

    Added PackageCreator for easy .unitypackage creating.

---
+++ Assets/Huawei/Editor/Utils/HMSUnityPackageCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace HmsPlugin
+{
+    public static class HMSUnityPackageCreator
+    {
+        [MenuItem("Huawei/Prepare Packages")]
+        private static void PreparePackages()
+        {
+            string[] hmsFileNames =
+            {
+                "Assets/Huawei",
+                "Assets/Plugins",
+                "Assets/Resources"
+            };
+            AssetDatabase.ExportPackage(hmsFileNames, Application.dataPath + "/HMSUnityPackageV2.unitypackage", ExportPackageOptions.Recurse | ExportPackageOptions.Interactive);
+        }
+    }
+}

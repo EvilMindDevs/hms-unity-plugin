1212bff14f62a83faef294934c377c2a87eebe96
Samet Güzel <sametguzeldev@gmail.com>
2021-04-26 18:33:52 +0300

    Updated Gradle Worker and used Unity's own gradle templates.

--- Assets/Huawei/Editor/Utils/HMSGradleFixer.cs
+++ Assets/Huawei/Editor/Utils/HMSGradleFixer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
 using UnityEditor.Android;
 using UnityEngine;
 
@@ -11,15 +12,27 @@ public class HMSGradleFixer : IPostGenerateGradleAndroidProject
     public void OnPostGenerateGradleAndroidProject(string path)
     {
         string fileName = "agconnect-services.json";
-        var filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
         string destPath = "";
-#if UNITY_2019
+#if UNITY_2019  || UNITY_2020
         destPath = Path.Combine(Directory.GetParent(path).FullName + "//launcher", fileName);
+
+        string mainTemplatePath = Application.dataPath + "/Plugins/Android/hmsMainTemplate.gradle";
+        FileUtil.CopyFileOrDirectory(mainTemplatePath, Path.GetFullPath(path) + @"/hmsMainTemplate.gradle");
+
+        string launcherTemplatePath = Application.dataPath + "/Plugins/Android/hmsLauncherTemplate.gradle";
+        FileUtil.CopyFileOrDirectory(launcherTemplatePath, Directory.GetParent(path).FullName + @"/launcher/hmsLauncherTemplate.gradle");
+
+        string baseProjectTemplatePath = Application.dataPath + "/Plugins/Android/hmsBaseProjectTemplate.gradle";
+        FileUtil.CopyFileOrDirectory(baseProjectTemplatePath, Directory.GetParent(path).FullName + @"/hmsBaseProjectTemplate.gradle");
+
 #elif UNITY_2018
         destPath = Path.Combine(path, fileName);
 #endif
         if (File.Exists(destPath))
-            File.Delete(destPath);
-        File.Copy(filePath, destPath);
+            FileUtil.DeleteFileOrDirectory(destPath);
+        FileUtil.CopyFileOrDirectory(filePath, destPath);
+
+
     }
 }

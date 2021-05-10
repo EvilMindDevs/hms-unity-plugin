ad06238e1c73b678009ab70561aff6cb6a56c8fd
Samet Güzel <sametguzeldev@gmail.com>
2021-05-11 02:54:24 +0300

    Prevented setting files getting overwritten when upgrading the plugin. Added "Check For Updates" menu item under Huawei. Added version controlling if there is a newer version released on github.

---
+++ Assets/Huawei/Editor/Utils/HMSPluginUpdaterInit.cs
@@ -0,0 +1,13 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+[InitializeOnLoad]
+public class HMSPluginUpdaterInit : AssetPostprocessor
+{
+    static HMSPluginUpdaterInit()
+    {
+        HMSPluginUpdater.Request();
+    }
+}
+
+#endif

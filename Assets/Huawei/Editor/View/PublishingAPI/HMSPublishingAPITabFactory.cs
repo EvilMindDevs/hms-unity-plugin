8b29e0ef42e673c0b6f13f6ff0c823e73099d410
Mustafa CAN <mcqn14@gmail.com>
2021-10-26 15:07:08 +0300

    HUP-30 done, but lacks proper progressbar, will add as new issue after developing progressbar

--- Assets/Huawei/Editor/View/PublishingAPI/HMSPublishingAPITabFactory.cs
+++ Assets/Huawei/Editor/View/PublishingAPI/HMSPublishingAPITabFactory.cs
@@ -1,25 +1,8 @@
 ﻿using HmsPlugin;
-using HmsPlugin.Button;
-using HmsPlugin.ConnectAPI;
-using HmsPlugin.Label;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
 using HmsPlugin.PublishingAPI;
 
 internal class HMSPublishingAPITabFactory
 {
-    private static string versionInfo = "";
-
-    static HMSPublishingAPITabFactory()
-    {
-        versionInfo = File.ReadAllText(Application.dataPath + "/Huawei/VERSION");
-    }
-
     public static TabView CreateQueryingAppInformationTab(TabBar tabBar)
     {
         var tab = new TabView("Query App Information");

9ffdfaef4e8d5b1fb5af92da3f967ba3e2b2a270
BNY\eymen <bunyamineymenalagoz@gmail.com>
2022-07-29 16:21:58 +0300

    HUP-82 : Dont open windows (3 IAP product window + Querying App Info Window) if token is null [Connect API]

--- Assets/Huawei/Editor/View/PMSWindow/CreateAProductWindow/HMSCreateAProductWindow.cs
+++ Assets/Huawei/Editor/View/PMSWindow/CreateAProductWindow/HMSCreateAProductWindow.cs
@@ -1,12 +1,16 @@
 ﻿using HmsPlugin;
 using HmsPlugin.Button;
 using HmsPlugin.Window;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using UnityEditor;
+
+using UnityEngine;
 
 public class HMSCreateAProductWindow : HMSEditorWindow
 {
@@ -14,6 +18,15 @@ public class HMSCreateAProductWindow : HMSEditorWindow
     public static void ShowProductWindow()
     {
         GetWindow(typeof(HMSCreateAProductWindow), false, "Create a Product").minSize = new UnityEngine.Vector2(400, 700);
+    }
+
+    // Validate the menu item defined by the function above.
+    // The menu item will be disabled if this function returns false.
+    [MenuItem("Huawei/Connect API/PMS API/Create a Product", true)]
+    static bool ValidatePMSAPIWindow()
+    {
+        Debug.Log("ValidatePMSAPIWindow");
+        return !string.IsNullOrEmpty(HMSConnectAPISettings.Instance.Settings.Get(HMSConnectAPISettings.AccessToken, ""));
     }
 
     public override IDrawer CreateDrawer()
--- Assets/Huawei/Editor/View/PublishingAPI/QueryingAppInformation/QueryingAppInfoWindow.cs
+++ Assets/Huawei/Editor/View/PublishingAPI/QueryingAppInformation/QueryingAppInfoWindow.cs
@@ -1,6 +1,9 @@
 using HmsPlugin;
 using HmsPlugin.Window;
+
 using UnityEditor;
+
+using UnityEngine;
 
 public class QueryingAppInformationWindow : HMSEditorWindow
 {
@@ -8,6 +11,15 @@ public class QueryingAppInformationWindow : HMSEditorWindow
     public static void ShowQueryingAppInformationWindow()
     {
         GetWindow(typeof(QueryingAppInformationWindow), false, "Querying App Information").minSize = new UnityEngine.Vector2(400, 700);
+    }
+
+    // Validate the menu item defined by the function above.
+    // The menu item will be disabled if this function returns false.
+    [MenuItem("Huawei/Connect API/Publishing API/Querying App Information", true)]
+    static bool ValidateShowQueryingAppInformationWindow()
+    {
+        Debug.Log("ValidateShowQueryingAppInformationWindow");
+        return !string.IsNullOrEmpty(HMSConnectAPISettings.Instance.Settings.Get(HMSConnectAPISettings.AccessToken, ""));
     }
 
     public override IDrawer CreateDrawer()

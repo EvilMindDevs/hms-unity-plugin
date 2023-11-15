236bbbfdc1ed36c62f84f7a3b6256817ddc541c7
Seyyid Yiğit <seyyid364@gmail.com>
2023-11-15 14:25:31 +0300

    Added Google Play Release area that  HMS Core Installer is  can be "Enabled" or "Disabled" in Common Libraries tab.
    - The toggle state is saved in the editor settings.
    - Some made improvements BaseProjectGradleFile function. First created all json file using StringBuilder and then writing file.

--- Assets/Huawei/Editor/View/LibrariesTab/HMSLibrariesDrawer.cs
+++ Assets/Huawei/Editor/View/LibrariesTab/HMSLibrariesDrawer.cs
@@ -1,30 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HmsPlugin
+﻿namespace HmsPlugin
 {
     public class HMSLibrariesDrawer : VerticalSequenceDrawer
     {
         private TabBar _tabBar;
         private TabView _tabView;
         private Toggle.Toggle _appSupportToggle;
+        private Toggle.Toggle _hmsCoreInstallToggle;
 
         public const string AppCompatEnabled = "AppCompat";
+        public const string GooglePlayReleaseEnabled = "GooglePlayRelease";
 
         public HMSLibrariesDrawer(TabBar tabBar)
         {
             _tabBar = tabBar;
             if (!HMSMainEditorSettings.Instance.Settings.HasKey(AppCompatEnabled)) 
                 HMSMainEditorSettings.Instance.Settings.SetBool(AppCompatEnabled, true);
+            if (!HMSMainEditorSettings.Instance.Settings.HasKey(GooglePlayReleaseEnabled)) 
+                HMSMainEditorSettings.Instance.Settings.SetBool(GooglePlayReleaseEnabled, true);
             _appSupportToggle = new Toggle.Toggle("App Compat (com.android.support:appcompat-v7:21.0.0)", HMSMainEditorSettings.Instance.Settings.GetBool(AppCompatEnabled, true), OnAppSupportToggleChanged, false).SetLabelWidth(350);
+            _hmsCoreInstallToggle = new Toggle.Toggle("HMS Core Installer (com.huawei.hms:hmscoreinstaller:6.6.0.300)", HMSMainEditorSettings.Instance.Settings.GetBool(GooglePlayReleaseEnabled, true), OnGooglePlayReleaseEnabledChanged, false).SetLabelWidth(375);
         }
+        
 
         private void OnAppSupportToggleChanged(bool value)
         {
             HMSMainEditorSettings.Instance.Settings.SetBool(AppCompatEnabled, value);
+        }
+
+        
+        private void OnGooglePlayReleaseEnabledChanged(bool value)
+        {
+            HMSMainEditorSettings.Instance.Settings.SetBool(GooglePlayReleaseEnabled, value);
         }
 
         public void CreateDrawer()
@@ -46,6 +52,14 @@ namespace HmsPlugin
             AddDrawer(new HorizontalSequenceDrawer(new HorizontalLine(), new Label.Label("Common Libraries").SetBold(true), new HorizontalLine()));
             AddDrawer(new HorizontalSequenceDrawer(new Spacer(), _appSupportToggle, new Spacer()));
             AddDrawer(new HorizontalLine());
+            AddDrawer(new Space(10));
+            AddDrawer(new HorizontalSequenceDrawer(new HorizontalLine(), new Label.Label("Google Play Release").SetBold(true), new HorizontalLine()));
+            AddDrawer(new HorizontalSequenceDrawer(new Spacer(), _hmsCoreInstallToggle, new Spacer()));
+            AddDrawer(new HorizontalLine());
+            AddDrawer(new Space(5));
+
+
+
         }
     }
 }

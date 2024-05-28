12ab858827eb1c30d56a3f1e6ebbcbd45f4e3b21
Seyyid Yiğit <seyyid364@gmail.com>
2024-05-28 14:17:23 +0300

    [HUP-861] feat: ML Kit base module integration
    - Added Gradle implementation
    - Added UI to  Kit settings panel
    - Added base scene
    
    Related Issues:
    - [HUP-860]: Main Task
    - [HUP-862]: Text-To-Speech Module Dev
    - [HUP-863]: Translate Module Dev

---
+++ Assets/Huawei/Editor/View/MLKitTab/HMSMLKitSettingsDrawer.cs
@@ -0,0 +1,98 @@
+using HmsPlugin.TextField;
+
+namespace HmsPlugin
+{
+
+    internal class HMSMLKitSettingsDrawer : VerticalSequenceDrawer
+    {
+        private bool translateIsActive = false;
+        private bool textToSpeechIsActive = false;
+        private TextField.TextFieldWithAccept _keyAPITextField;
+
+        private Toggle.Toggle _enableTranslateToggle;
+        private Toggle.Toggle _enableTextToSpeechToggle;
+        private HMSSettings _settings;
+
+        public HMSMLKitSettingsDrawer()
+        {
+            _settings = HMSMLKitSettings.Instance.Settings;
+
+            _keyAPITextField = new TextFieldWithAccept("KeyAPI", _settings.Get(HMSMLKitSettings.MLKeyAPI),
+                "Save", OnKeyAPISaveButtonClick).SetLabelWidth(0).SetButtonWidth(100);
+
+            translateIsActive = _settings.GetBool(HMSMLKitSettings.EnableTranslateModule);
+            textToSpeechIsActive = _settings.GetBool(HMSMLKitSettings.EnableTextToSpeechModule);
+            AddDrawer(new VerticalSequenceDrawer(
+                new HorizontalSequenceDrawer(new Spacer(), new Label.Label("- ML Kit Modules -").SetBold(true), new Spacer()),
+                new HorizontalSequenceDrawer(new HorizontalLine())
+            ));
+
+            SetupSequence();
+        }
+
+        private void TranslateModuleDrawer()
+        {
+            AddDrawer(new VerticalSequenceDrawer(
+                new HorizontalSequenceDrawer(new Label.Label("Translate Module").SetBold(true)),
+                new HorizontalSequenceDrawer(new Spacer()),
+                new HorizontalSequenceDrawer(new Label.Label("Translate Module enables you to translate text between 50+ languages."))
+            ));
+            AddDrawer(new Space(3));
+
+            _enableTranslateToggle = new Toggle.Toggle("Enable Translate Module", translateIsActive, OnTranslateToggleChanged, false);
+            AddDrawer(_enableTranslateToggle);
+            AddDrawer(new HorizontalLine());
+
+        }
+
+        private void TextToSpeechModuleDrawer()
+        {
+            AddDrawer(new VerticalSequenceDrawer(
+                new HorizontalSequenceDrawer(new Label.Label("Text To Speech Module").SetBold(true)),
+                new HorizontalSequenceDrawer(new Spacer()),
+                new HorizontalSequenceDrawer(new Label.Label("Text To Speech Module enables you to convert text to speech in 50+ languages."))
+            ));
+            _enableTextToSpeechToggle = new Toggle.Toggle("Enable Text To Speech Module", textToSpeechIsActive, OnTextToSpeechToggleChanged, false);
+            AddDrawer(_enableTextToSpeechToggle);
+            AddDrawer(new HorizontalLine());
+        }
+
+        private void KeyAPIDrawer()
+        {
+            AddDrawer(new VerticalSequenceDrawer(
+                new HorizontalSequenceDrawer(new Label.Label("Key API").SetBold(true)),
+                new HorizontalSequenceDrawer(new Spacer()),
+                new HorizontalSequenceDrawer(new Label.Label("You can get your API key from Huawei Developer Console OR  agconnect-services.json file."))
+            ));
+            AddDrawer(new Space(3));
+            AddDrawer(_keyAPITextField);
+            AddDrawer(new HorizontalLine());
+        }
+
+        private void SetupSequence()
+        {
+            KeyAPIDrawer();
+            TranslateModuleDrawer();
+            TextToSpeechModuleDrawer();
+        }
+        private void OnTranslateToggleChanged(bool value)
+        {
+            translateIsActive = value;
+
+            _settings.SetBool(HMSMLKitSettings.EnableTranslateModule, translateIsActive);
+        }
+
+        private void OnTextToSpeechToggleChanged(bool value)
+        {
+            textToSpeechIsActive = value;
+            _settings.SetBool(HMSMLKitSettings.EnableTextToSpeechModule, textToSpeechIsActive);
+        }
+
+        private void OnKeyAPISaveButtonClick()
+        {
+            _settings.Set(HMSMLKitSettings.MLKeyAPI, _keyAPITextField.GetCurrentText());
+        }
+    }
+
+
+}
